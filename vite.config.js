768abf7c8ad2235e48854f6e51edbfa91a10c1d6
shivangisharma4 <shivangi.sharma7004@gmail.com>
2025-06-14 20:56:47 +0530

    hosting again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or '/your-repo-name/' if hosting from a repo subpath
+  base: '/',
 })
